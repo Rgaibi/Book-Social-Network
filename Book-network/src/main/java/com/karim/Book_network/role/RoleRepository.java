1718b6d2e6ffabed2d99728590a3ff3ad11f1e69
karim rgaibi <karimrgaibi@gmail.com>
2025-11-16 22:07:34 +0100

    add role folder: Role and RoleRepository

---
+++ Book-network/src/main/java/com/karim/Book_network/role/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.karim.Book_network.role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+    Optional<Role> findByName(String name);
+
+
+}
