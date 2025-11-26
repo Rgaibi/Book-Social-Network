8958ea20aff5c2db7691a1c81b654c9928d19358
karim rgaibi <karimrgaibi@gmail.com>
2025-11-26 20:23:23 +0100

    Implement exception handler for LockedException

--- Book-network/src/main/java/com/karim/Book_network/handler/GlobalExceptionHandler.java
+++ Book-network/src/main/java/com/karim/Book_network/handler/GlobalExceptionHandler.java
@@ -1,7 +1,28 @@
 package com.karim.Book_network.handler;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.LockedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    
+    @ExceptionHandler(LockedException.class)
+    public ResponseEntity<ExceptionResponse> handleException (LockedException exp) {
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(ExceptionResponse.builder()
+                        .businessErrorCode(BusinessErrorCodes.ACCOUNT_LOCKED.getCode())
+                        .businessErrorDescription(BusinessErrorCodes.ACCOUNT_LOCKED.getDescription())
+                        .error(exp.getMessage())
+                        .build()
+                );
+    }
+
+
+
 }
