b66db3562d79471da0f2f7fe90341226573bc52e
karim rgaibi <karimrgaibi@gmail.com>
2025-11-26 20:36:19 +0100

    Implement exception handler for DisabledException

--- Book-network/src/main/java/com/karim/Book_network/handler/GlobalExceptionHandler.java
+++ Book-network/src/main/java/com/karim/Book_network/handler/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.karim.Book_network.handler;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.LockedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    
+
     @ExceptionHandler(LockedException.class)
     public ResponseEntity<ExceptionResponse> handleException (LockedException exp) {
         return ResponseEntity
@@ -22,6 +23,20 @@ public class GlobalExceptionHandler {
                         .build()
                 );
     }
+
+    @ExceptionHandler(DisabledException.class)
+    public ResponseEntity<ExceptionResponse> handleException (DisabledException exp) {
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(ExceptionResponse.builder()
+                        .businessErrorCode(BusinessErrorCodes.ACCOUNT_DISABLED.getCode())
+                        .businessErrorDescription(BusinessErrorCodes.ACCOUNT_DISABLED.getDescription())
+                        .error(exp.getMessage())
+                        .build()
+                );
+    }
+
+    
 
 
 
