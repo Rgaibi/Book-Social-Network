8ea07e59147e5ce1bca3d5004dc6ef2f3be178b7
karim rgaibi <karimrgaibi@gmail.com>
2025-11-29 20:28:32 +0100

    Implementing saveBook en BookController

--- Book-network/src/main/java/com/karim/Book_network/book/BookController.java
+++ Book-network/src/main/java/com/karim/Book_network/book/BookController.java
@@ -1,7 +1,12 @@
 package com.karim.Book_network.book;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Tag(name = "Book")
 public class BookController {
+
+    private  final BookService service;
+
+    @PostMapping
+    public ResponseEntity<Integer> saveBook(@Valid @RequestBody BookRequest request, Authentication connectedUser) {
+        return ResponseEntity.ok(service.save(request, connectedUser));
+    }
 }
