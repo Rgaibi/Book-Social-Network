0c8ba818820a51cb4c3ce1f78b77beb28cbf35a5
karim rgaibi <karimrgaibi@gmail.com>
2025-12-01 17:51:59 +0100

    Implement GET endpoint to fetch a book by ID

--- Book-network/src/main/java/com/karim/Book_network/book/BookController.java
+++ Book-network/src/main/java/com/karim/Book_network/book/BookController.java
@@ -5,10 +5,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("books")
@@ -22,4 +19,11 @@ public class BookController {
     public ResponseEntity<Integer> saveBook(@Valid @RequestBody BookRequest request, Authentication connectedUser) {
         return ResponseEntity.ok(service.save(request, connectedUser));
     }
+
+    @GetMapping("{book-id}")
+    public ResponseEntity<BookResponse> findBookById(@PathVariable("book-id") Integer bookId) {
+        return ResponseEntity.ok(service.findById(bookId));
+    }
+
+
 }
