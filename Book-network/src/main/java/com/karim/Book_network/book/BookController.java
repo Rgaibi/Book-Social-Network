d1ff9455881d28990253a4dc9e04a3b63fa45aee
karim rgaibi <karimrgaibi@gmail.com>
2025-12-01 18:56:38 +0100

    implement GET /books with pagination and authentication

--- Book-network/src/main/java/com/karim/Book_network/book/BookController.java
+++ Book-network/src/main/java/com/karim/Book_network/book/BookController.java
@@ -1,5 +1,6 @@
 package com.karim.Book_network.book;
 
+import com.karim.Book_network.common.PageResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -23,6 +24,15 @@ public class BookController {
     @GetMapping("{book-id}")
     public ResponseEntity<BookResponse> findBookById(@PathVariable("book-id") Integer bookId) {
         return ResponseEntity.ok(service.findById(bookId));
+    }
+
+    @GetMapping
+    public  ResponseEntity<PageResponse<BookResponse>> findAllBooks(
+            @RequestParam(name = "page", defaultValue = "0", required = false) int page,
+            @RequestParam(name = "size", defaultValue = "10", required = false) int size,
+            Authentication connectedUser
+    ) {
+        return ResponseEntity.ok(service.findAllBooks(page, size, connectedUser));
     }
 
 
