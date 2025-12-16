f7cf6f422e84c1b82615fa4a7d7f66cd8e683277
karim rgaibi <karimrgaibi@gmail.com>
2025-12-16 23:34:20 +0100

    feat(auth): implement register flow and redirect to activation

--- book-network-ui/src/app/pages/register/register.component.ts
+++ book-network-ui/src/app/pages/register/register.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { RegistrationRequest } from 'src/app/api/models';
+import { AuthenticationService } from 'src/app/api/services/authentication.service';
 
 @Component({
   selector: 'app-register',
@@ -12,12 +14,25 @@ export class RegisterComponent {
   registerRequest: RegistrationRequest = {email: '', password: '', firstname: '', lastname: ''};
   errorMsg: Array<string> = [];
 
-  login() {
+  private router = inject(Router);
+  private authService = inject(AuthenticationService)
 
+  login() {
+    this.router.navigate(['/login']);
   }
 
-  
-  register(_t7: NgForm) {
+
+  register(form: NgForm) {
+    this.errorMsg = [];
+
+    this.authService.register({ body: this.registerRequest }).subscribe({
+        next: () => {
+          this.router.navigate(['/activate-account']);
+        },
+        error: (err) => {
+          this.errorMsg = err.error.validationErrors;
+        }
+    })
 
   }
 
