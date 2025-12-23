641775e4dc55b2f65248d7253c2b5ff80591a582
karim rgaibi <karimrgaibi@gmail.com>
2025-12-23 18:06:36 +0100

    feat: add menu navbar HTML structure with Font Awesome icons

---
+++ book-network-ui/src/app/modules/components/menu/menu.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent {
+logout() {
+throw new Error('Method not implemented.');
+}
+search() {
+throw new Error('Method not implemented.');
+}
+
+}
